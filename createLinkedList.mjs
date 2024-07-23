87cce40a2ae63ad9ff65591b595519454a0ce9f5
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-23 16:19:58 +1000

    add append method to add nodes to the end of a linked list

--- createLinkedList.mjs
+++ createLinkedList.mjs
@@ -1,3 +1,17 @@
+import createNode from "./createNode.mjs";
+
 export default function createLinkedList() {
-  return;
+  let headNode = null;
+
+  const append = (val) => {
+    const newNode = createNode(val);
+
+    if (headNode === null) return (headNode = newNode);
+
+    let currentNode = headNode;
+    while (currentNode.nextNode !== null) currentNode = currentNode.nextNode;
+    currentNode.nextNode = newNode;
+  };
+
+  return { append };
 }
