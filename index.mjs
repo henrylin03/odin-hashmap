8234b5e52cde61b575ea8f97eef1070c4bc1279b
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-23 16:25:19 +1000

    remove tests for linkedlist implementation in #3

--- index.mjs
+++ index.mjs
@@ -1,10 +1,4 @@
 import createHashMap from "./createHashMap.mjs";
-import createLinkedList from "./createLinkedList.mjs";
 
 const hashMap = createHashMap();
 console.log(hashMap.hash("heooll"));
-
-const list = createLinkedList();
-list.append(1);
-list.append(2);
-list.print();
