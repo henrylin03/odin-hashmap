7eb696eacea9e27802ff2cc308942f4c56a58d47
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-23 15:37:42 +1000

    use modulo of bucket size (which might need to grow, so is not a constant) to handle javascript's inability to hold large numbers precisely (which increases changes of collisions)

--- createHashMap.mjs
+++ createHashMap.mjs
@@ -2,9 +2,10 @@ export default function createHashMap() {
   const hash = (key) => {
     let hashCode = 0;
     const PRIME_NUMBER = 31;
-    
+    let bucketSize = 16;
+
     for (let i = 0; i < key.length; i++) {
-      hashCode = PRIME_NUMBER * hashCode + key.charCodeAt(i);
+      hashCode = (PRIME_NUMBER * hashCode + key.charCodeAt(i)) % bucketSize;
     }
 
     return hashCode;
