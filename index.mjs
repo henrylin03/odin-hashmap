e966089ab5eea12d9a35893f04b3b260d6a0c528
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-24 17:24:09 +1000

    remove unused test now

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import createHashMap from "./createHashMap.mjs";
 
 const hashMap = createHashMap();
-console.log(hashMap.hash("heooll"));
+
 console.log(hashMap.set("harry", "potter"));
 console.log(hashMap.set("Severus", "Snape"));
 console.log(hashMap.set("harry", "the prince")); // previous entry should be overwritten
