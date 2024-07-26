0bc2808362163ba92cc08b9a2755b6dded295ea5
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-26 17:00:32 +1000

    update test case

--- index.mjs
+++ index.mjs
@@ -7,6 +7,6 @@ hashMap.set("arrhy", "what is this");
 hashMap.set("Severus", "Snape");
 hashMap.set("harry", "the prince");
 
-console.log(hashMap.entries()); // expected: [["harry", "the prince"], ["arrhy", "what is this"], ["Severus", "Snape"]]
+console.log(hashMap.keys()); // expected: ["harry", "arrhy", "Severus"]
 hashMap.remove("harry");
-console.log(hashMap.entries()); // expected: [["arrhy", "what is this"], ["Severus", "Snape"]]
+console.log(hashMap.keys()); // expected: ["arrhy", "Severus"]
