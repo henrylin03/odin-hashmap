369fe97c2dae49dbadbe48357f4037c005e8ccbd
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-07-27 12:12:40 +1000

    update test console log statements for clarity

--- index.mjs
+++ index.mjs
@@ -6,7 +6,14 @@ hashMap.set("harry", "potter");
 hashMap.set("arrhy", "what is this");
 hashMap.set("Severus", "Snape");
 hashMap.set("harry", "the prince");
+
+console.log(`
+The current bucket size is ${hashMap.getBucketSize()}`);
+hashMap.print();
+
 hashMap.set("hermione", "granger");
 hashMap.set("ron", "weasley");
 
+console.log(`
+The bucket size is now ${hashMap.getBucketSize()}`);
 hashMap.print();
